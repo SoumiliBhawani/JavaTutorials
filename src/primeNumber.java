f0b1bb48fa6ab60b9c55e225d6ecc48dda5b3451
Soumili Bhawani <s1bhawani@gmail.com>
2026-04-30 14:06:45 +0530

    add prime number

---
+++ src/primeNumber.java
@@ -0,0 +1,23 @@
+public class primeNumber {
+    public static void main(String[] args) {
+        int number = 17; // Test case 1
+
+        // Check if number is prime
+        // Print: Is prime: <true/false>
+        boolean isPrime = true;
+        if(number <= 1) {
+            isPrime = false;
+        }
+        else {
+            for(int i = 2; i <= (number/2); i++) {
+                if(number % i == 0) {
+                    isPrime = false;
+                    break; // Exit loop when divisor is found
+                }
+            }
+        }
+        System.out.println("Is prime: " + isPrime);
+    }
+}
+
+

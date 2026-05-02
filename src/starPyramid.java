af54f9fafda29400cc12a6c25d5a75db7ad85fb3
Soumili Bhawani <s1bhawani@gmail.com>
2026-05-02 12:38:41 +0530

    add

---
+++ src/starPyramid.java
@@ -0,0 +1,25 @@
+public class starPyramid {
+
+        public static void main(String[] args) {
+            int n = 3; // Test case 1
+            for (int i = 1; i <= n; i++) {
+                // print spaces
+                for (int j = 1; j <= n - i; j++) {
+                    System.out.print(" ");
+                }
+
+                // print stars
+                for (int k = 1; k <= (2 * i - 1); k++) {
+                    System.out.print("*");
+                }
+
+                System.out.println();
+            }
+
+            // Print centered pyramid:
+            //   *
+            //  ***
+            // *****
+        }
+    }
+
